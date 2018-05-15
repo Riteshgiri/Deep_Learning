a84ab8abb457514d8070754c290226bbe26ce472
RIteshgiri <krketrock1@gmail.com>
2018-05-15 15:10:10 +0800

    Import All Important Packages Load the Data_Set (testcatvnoncat & traincatvnoncat)

--- Logistic Regression -Neural Nwk/Shapes_Of_Datasets.py
+++ Logistic Regression -Neural Nwk/Shapes_Of_Datasets.py
@@ -1,13 +1,12 @@
 import numpy as np
-import scipy
-import h5py
 import matplotlib.pyplot as plt
+import h5py
+import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
-index = 77
-plt.imshow(train_set_x_orig[index])
-print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+print("No of training set images : train_set_x_orig :" + str(train_set_x_orig.shape[0]))
+print("No of test set images : test_set_x_orig :" + str(test_set_x_orig.shape[0]))
